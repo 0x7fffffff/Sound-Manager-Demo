9d3a16f301b95677a26ae087af2b9c62a3cd634d
Michael MacCallum <micksmaccallum@gmail.com>
2016-01-13 12:42:48 -0500

    Initial Commit

---
+++ Sound Manager Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sound Manager Demo
+//
+//  Created by Michael MacCallum on 1/13/16.
+//  Copyright © 2016 Michael MacCallum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
