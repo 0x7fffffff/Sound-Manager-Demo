ca0be2ae33c4734f3f2a0f560a3e92c661b390c2
Michael MacCallum <micksmaccallum@gmail.com>
2016-01-13 23:50:07 -0500

    Added ControlView class

---
+++ Sound Manager Demo/ControlView.h
@@ -0,0 +1,13 @@
+//
+//  ControlView.h
+//  Sound Manager Demo
+//
+//  Created by Michael MacCallum on 1/13/16.
+//  Copyright © 2016 Michael MacCallum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ControlView : UIView
+
+@end
---
+++ Sound Manager Demo/ControlView.m
@@ -0,0 +1,21 @@
+//
+//  ControlView.m
+//  Sound Manager Demo
+//
+//  Created by Michael MacCallum on 1/13/16.
+//  Copyright © 2016 Michael MacCallum. All rights reserved.
+//
+
+#import "ControlView.h"
+
+@implementation ControlView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
