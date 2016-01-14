61ca545eaaa5fefe6ab1d28055d072638a8a00de
Michael MacCallum <micksmaccallum@gmail.com>
2016-01-13 23:48:49 -0500

    Version checking

---
+++ Sound Manager Demo/VersionChecks.h
@@ -0,0 +1,15 @@
+//
+//  VersionChecks.h
+//  Sound Manager Demo
+//
+//  Created by Michael MacCallum on 1/13/16.
+//  Copyright © 2016 Michael MacCallum. All rights reserved.
+//
+
+// http://stackoverflow.com/a/5337804/716216
+#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+
