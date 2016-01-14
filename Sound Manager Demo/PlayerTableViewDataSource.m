c8b31dfcf97de902fbcba5dbafa553cb2e85a05f
Michael MacCallum <micksmaccallum@gmail.com>
2016-01-14 13:33:12 -0500

    Moved tableview data source into a dedicated object

--- Sound Manager Demo/PlayerTableViewDataSource.m
+++ Sound Manager Demo/PlayerTableViewDataSource.m
@@ -7,7 +7,28 @@
 //
 
 #import "PlayerTableViewDataSource.h"
+#import "PlayerTableViewCell.h"
+#import "Sound.h"
+#import "SoundManager.h"
 
 @implementation PlayerTableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    NSLog(@"%lu", [SoundManager sharedManager].availableSounds.count);
+    return [SoundManager sharedManager].availableSounds.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    PlayerTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PlayerCellIdentifier"
+                                                                forIndexPath:indexPath];
+    Sound *currentSound = [[SoundManager sharedManager].availableSounds objectAtIndex:indexPath.row];
+    //    cell.textLabel.text = currentSound.fileName;
+    cell.nameLabel.text = currentSound.fileName;
+    [cell layoutIfNeeded];
+    
+    return cell;
+}
 
 @end
